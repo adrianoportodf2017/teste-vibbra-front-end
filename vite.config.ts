4b9e57a5e702e36f6bfac615ad4eae4772b35f70
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2025-08-22 00:49:49 -0300

    subvindo

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
- import tailwindcss from '@tailwindcss/vite'
-
+import react from '@vitejs/plugin-react'
+ 
 export default defineConfig({
-  plugins: [
-    tailwindcss({
-      // Especificar explicitamente se necessário
-      config: './tailwind.config.js'
-    }),  ],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+       ],
+    },
+  },
 })
