ff625eb94a41a75f13e182f7d1086663b95201c0
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2025-08-22 01:22:36 -0300

    correção da api

--- src/services/http.ts
+++ src/services/http.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost/api',
+  baseURL: 'http://api-teste-vibbra.agenciatecnet.com.br/api',
   timeout: 10000,
 });
 
