7b64af82868b7b51a051fbde904c5c136089d4de
adriano alves <33916451+adrianoportodf2017@users.noreply.github.com>
2025-08-22 00:01:16 -0300

    correcoes de erros

--- src/types/invites.ts
+++ src/types/invites.ts
@@ -1,3 +1,3 @@
-export interface Invite { id?: number; name: string; email: string; user: number; user_invited: number; }
+export interface Invite { id?: number; name: string; email: string; user: number; user_invited: number;  status: 'pending' | 'accepted' | 'rejected';}
 export interface CreateInviteData { name: string; email: string; user: number; user_invited: number; }
 export interface InviteResponse { invite: Invite; }
